1fd22883ee4fc6bc42bff34c7b3bdad29827fa16
nikolaimoiseev01 <nikolaimoiseev01@gmail.com>
2026-07-26 22:06:10 +0300

    Initial Commit

--- resources/views/components/page-blocks/key_figure.blade.php
+++ resources/views/components/page-blocks/key_figure.blade.php
@@ -25,32 +25,34 @@
 
 @if($style === 'card_blue')
     <div x-data="revealOnScroll()" class="page-block page-block--key-figure rounded-2xl p-8 flex flex-col justify-between items-stretch {{ $spacingTop }} {{ $spacingBottom }} {{colorHelper('bg_color', $data)}}">
-        @if(trim(strip_tags($data['context'])) !== '')
-            <div class="text-sm leading-relaxed mb-4 {{colorHelper('context_color', $data)}}">{!! $data['context'] !!}</div>
-        @endif
-        <div class="flex flex-col justify-center">
+        <div class="!mb-4 flex flex-col justify-center">
             <p class="text-[80px] leading-[80px] font-normal mb-2 {{colorHelper('main_color', $data)}}">{{ $data['value'] }}</p>
             <h3 class="mb-0 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</h3>
         </div>
+        @if(trim(strip_tags($data['context'])) !== '')
+            <div class="text-sm leading-relaxed mb-4 {{colorHelper('context_color', $data)}}">{!! $data['context'] !!}</div>
+        @endif
     </div>
 
 @elseif($style === 'card_light')
     <div x-data="revealOnScroll()" class="page-block page-block--key-figure rounded-2xl bg-[#F7F9FC] border border-[#E1E7F0] p-8 flex flex-col justify-between items-stretch {{ $spacingTop }} {{ $spacingBottom }}">
-        @if(trim(strip_tags($data['context'] ?? '')) !== '')
-            <div class="text-sm leading-relaxed mb-4 {{colorHelper('context_color', $data)}}">{!! $data['context'] ?? '' !!}</div>
-        @endif
-        <div class="flex flex-col justify-center">
+        <div class="!mb-4 flex flex-col justify-center">
             <p class="text-[80px] leading-[80px] font-normal mb-2 {{colorHelper('main_color', $data)}}">{{ $data['value'] }}</p>
             <h3 class="mb-0 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</h3>
         </div>
+        @if(trim(strip_tags($data['context'] ?? '')) !== '')
+            <div class="text-sm leading-relaxed mb-4 {{colorHelper('context_color', $data)}}">{!! $data['context'] ?? '' !!}</div>
+        @endif
     </div>
 
 @elseif($style === 'inline_large')
     <div x-data="revealOnScroll()" class="page-block page-block--key-figure text-center py-8 flex flex-col items-center justify-center {{ $spacingTop }} {{ $spacingBottom }}">
         <p class="text-[80px] leading-[80px] font-normal mb-3 {{colorHelper('main_color', $data)}}">{{ $data['value'] }}</p>
-        <h3 class="mb-0 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</h3>
+        <h3 class="!mb-4 mb-0 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</h3>
         @if(trim(strip_tags($data['context'] ?? '')) !== '')
-            <div class="text-sm leading-relaxed mt-4 max-w-xl mx-auto {{colorHelper('context_color', $data)}}">{!! $data['context'] ?? '' }}</div>
+            <div class="text-sm leading-relaxed mt-4 max-w-xl mx-auto {{ colorHelper('context_color', $data) }}">
+                {!! $data['context'] ?? '' !!}
+            </div>
         @endif
     </div>
 
@@ -58,7 +60,7 @@
     <div x-data="revealOnScroll()" class="page-block page-block--key-figure flex items-center gap-8 py-4 lg:flex-col lg:items-start min-h-[80px] {{ $spacingTop }} {{ $spacingBottom }}">
         <p class="text-[80px] leading-[80px] font-normal shrink-0 {{colorHelper('main_color', $data)}}">{{ $data['value'] }}</p>
         <div>
-            <p class="font-bold mb-1 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</p>
+            <p class="!mb-4 font-bold mb-1 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</p>
             @if(!empty($data['context'] ?? ''))
                 <div class="text-sm leading-relaxed {{colorHelper('context_color', $data)}}">{!! $data['context'] ?? '' !!}</div>
             @endif
@@ -68,9 +70,11 @@
 @elseif($style === 'accent_border')
     <div x-data="revealOnScroll()" class="page-block page-block--key-figure border-l-4 pl-6 py-4 flex flex-col justify-center {{ $spacingTop }} {{ $spacingBottom }} {{colorHelper('main_color', $data)}}" style="border-color: currentColor">
         <p class="text-[80px] leading-[80px] font-normal mb-2 {{colorHelper('main_color', $data)}}">{{ $data['value'] }}</p>
-        <h3 class="mb-0 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</h3>
+        <h3 class="!mb-4 mb-0 {{colorHelper('text_color', $data)}}">{{ $data['description'] }}</h3>
         @if(trim(strip_tags($data['context'] ?? '')) !== '')
-            <div class="text-sm leading-relaxed mt-2 {{colorHelper('context_color', $data)}}">{!! $data['context'] ?? '' }}</div>
+            <div class="text-sm leading-relaxed mt-4 max-w-xl mx-auto {{ colorHelper('context_color', $data) }}">
+                {!! $data['context'] ?? '' !!}
+            </div>
         @endif
     </div>
 @endif
