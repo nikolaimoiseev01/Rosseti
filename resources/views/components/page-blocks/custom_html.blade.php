7367026f7e648f7b0c1db7d13f3d2be186f587b6
Anton Klimov <anton.e.klimov@gmail.com>
2026-08-03 08:44:18 +0300

    fix: prevent custom_html shadow DOM content from expanding page width

--- resources/views/components/page-blocks/custom_html.blade.php
+++ resources/views/components/page-blocks/custom_html.blade.php
@@ -39,7 +39,7 @@
     <div id="{{ $blockId }}"
          x-data="revealOnScroll()"
          class="page-block page-block--custom-html {{ $spacingTop }} {{ $spacingBottom }}"
-         style="{{ $widthStyle }}"
+         style="{{ $widthStyle }}; overflow: hidden"
     >
         <template id="tpl-{{ $blockId }}">
             @if(!empty($cssContent))
