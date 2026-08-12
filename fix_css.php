0b6ab9cf00493e88d0c447849b368228654119fa
Anton Klimov <anton.e.klimov@gmail.com>
2026-08-12 15:15:22 +0300

    Revert "chore: rebuild assets, clear view cache for key_figure decimal fix"
    This reverts commit 009d5abf9dbc60558573df09d32791810d48d8fa.

--- fix_css.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-echo 'Total blocks: ' . \App\Models\Block::count() . PHP_EOL;
-echo 'Types: ' . implode(', ', \App\Models\Block::pluck('type')->unique()->toArray()) . PHP_EOL;
